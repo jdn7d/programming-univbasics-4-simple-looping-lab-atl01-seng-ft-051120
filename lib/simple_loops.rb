0e655344844865271337e7ae8d2785e3ab34337f
Jessie Donovan <donovanajessie@gmail.com>
2020-05-11 01:40:12 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,7 +1,16 @@
+
 def loop_message_five_times(message)
 counter = 0
-while message[counter]do
-  puts message[counter]
+while counter< 5 do
+  puts message
   counter+= 1
+end
+end
+
+def loop_message_n_times(message, number)
+counter = 0
+while counter< number do
+  puts message
+  counter +=1
 end
 end
